479e46cb04dd6446124b577306d205beabc74658
Your Name <you@example.com>
2022-08-30 15:05:25 +0900

    User Logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Profile() {
+  const auth = getAuth();
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
+
+  // useEffect(() => {
+  //   setUser(auth.currentUser);
+  // }, []);
+
   return (
-    <div>Profile</div>
-  )
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
