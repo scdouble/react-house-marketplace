f351c787803e8d17f79e7abd246d569f451bb3e3
Your Name <you@example.com>
2022-08-31 21:10:21 +0900

    Listing Details

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,10 +5,11 @@ import bedIcon from '../assets/svg/bedIcon.svg';
 import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 
 const ListingItem = ({ listing, id, onDelete }) => {
+  console.log(listing)
   return (
     <li className="categoryListing">
       <Link to={`/category/${listing.type}/${id}`} className="categoryListingLink">
-        <img src={listing.imageUrls[0]} alt={listing.name} className="categoryListingImg" />
+        <img src={listing.imgUrls[0]} alt={listing.name} className="categoryListingImg" />
         <div className="categoryListingDetails">
           <p className="categoryListingLocation">{listing.location}</p>
           <p className="categoryListingName">{listing.name}</p>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import { db } from '../firebase.config';
-import ShareIcon from '../assets/svg/shareIcon.svg';
+import shareIcon from '../assets/svg/shareIcon.svg';
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -26,10 +26,64 @@ function Listing() {
         setLoading(false);
       }
     };
-    fetchListing()
-  },[navigate, params.listingId]);
+    fetchListing();
+  }, [navigate, params.listingId]);
 
-  return <div>Listing</div>;
+  if (loading) {
+    return <Spinner></Spinner>;
+  }
+  return (
+    <main>
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $ {listing.offer ? listing.discountedPrice : listing.regularPrice}
+        </p>
+
+        <p className="listingLocation">{listing.location}</p>
+
+        <p className="listingType">For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+
+        {listing.offer && (
+          <p className="discountedPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `1 Bedroom`}</li>
+          <li>{listing.bathroom > 1 ? `${listing.bathroom} Bathrooms` : `1 Bathroom`}</li>
+          <li>{listing.parking && 'Parking Sopt'}</li>
+          <li>{listing.furnished && 'Parking Sopt'}</li>
+          <p className="listingLocationTitle">Location</p>
+          {/* map */}
+
+          {auth.currentUser?.uid !== listing.userRef && (
+            <Link
+              to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+              className="primaryButton"
+            >
+              Contact Landlord
+            </Link>
+          )}
+        </ul>
+      </div>
+    </main>
+  );
 }
 
 export default Listing;
