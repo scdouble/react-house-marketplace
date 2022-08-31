9371f933d7fddf039257a129e7bcc5bc11a54146
Your Name <you@example.com>
2022-08-31 20:35:02 +0900

    Quick Change

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import Spinner from '../components/Spinner';
-import { toast } from 'react-toastify';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { db } from '../firebase.config';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { v4 as uuidv4 } from 'uuid';
+import Spinner from '../components/Spinner';
 
 function CreateListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -18,8 +18,8 @@ function CreateListing() {
     bedrooms: 1,
     bathrooms: 1,
     parking: false,
-    address: '',
     furnished: false,
+    address: '',
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
@@ -58,6 +58,7 @@ function CreateListing() {
         }
       });
     }
+
     return () => {
       isMounted.current = false;
     };
@@ -94,23 +95,19 @@ function CreateListing() {
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
 
-      location =
-        data.status === 'ZERO_RESULTS'
-          ? undefined
-          : data.results[0]?.formatted_address;
+      location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address;
 
       if (location === undefined || location.includes('undefined')) {
         setLoading(false);
-        toast.error('please enter a correct address');
+        toast.error('Please enter a correct address');
         return;
       }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
-    // Store images in firebase
+    // Store image in firebase
     const storeImage = async (image) => {
       return new Promise((resolve, reject) => {
         const storage = getStorage();
@@ -120,16 +117,9 @@ function CreateListing() {
 
         const uploadTask = uploadBytesResumable(storageRef, image);
 
-        // https://firebase.google.com/docs/storage/web/upload-files?hl=ja
-        // Register three observers:
-        // 1. 'state_changed' observer, called any time the state changes
-        // 2. Error observer, called on failure
-        // 3. Completion observer, called on successful completion
         uploadTask.on(
           'state_changed',
           (snapshot) => {
-            // Observe state change events such as progress, pause, and resume
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             console.log('Upload is ' + progress + '% done');
             switch (snapshot.state) {
@@ -144,14 +134,12 @@ function CreateListing() {
             }
           },
           (error) => {
-            // Handle unsuccessful uploads
             reject(error);
           },
           () => {
             // Handle successful uploads on complete
             // For instance, get the download URL: https://firebasestorage.googleapis.com/...
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              console.log('File available at', downloadURL);
               resolve(downloadURL);
             });
           },
@@ -159,9 +147,10 @@ function CreateListing() {
       });
     };
 
-    const imgUrls = await Promise.all([...images].map((image) => storeImage(image))).catch(() => {
+    const imgUrls = await Promise.all([...images].map((image) => storeImage(image))).catch((e) => {
       setLoading(false);
-      toast.error('Image not uploaded');
+      console.error(e)
+      toast.error('Images not uploaded');
       return;
     });
 
@@ -172,12 +161,10 @@ function CreateListing() {
       timestamp: serverTimestamp(),
     };
 
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
-
-    console.log(formDataCopy);
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
     setLoading(false);
@@ -187,6 +174,7 @@ function CreateListing() {
 
   const onMutate = (e) => {
     let boolean = null;
+
     if (e.target.value === 'true') {
       boolean = true;
     }
@@ -196,22 +184,25 @@ function CreateListing() {
 
     // Files
     if (e.target.files) {
-      setFormData((prevState) => {
-        return { ...prevState, images: e.target.files };
-      });
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
     }
 
-    // Text
+    // Text/Booleans/Numbers
     if (!e.target.files) {
-      setFormData((prevState) => {
-        return { ...prevState, [e.target.id]: boolean ?? e.target.value };
-      });
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
     }
   };
 
   if (loading) {
     return <Spinner />;
   }
+
   return (
     <div className="profile">
       <header>
@@ -244,25 +235,23 @@ function CreateListing() {
 
           <label className="formLabel">Name</label>
           <input
-            type="text"
             className="formInputName"
+            type="text"
             id="name"
             value={name}
             onChange={onMutate}
             maxLength="32"
-            required
             minLength="10"
+            required
           />
 
           <div className="formRooms flex">
             <div>
-              <label htmlFor="" className="formLabel">
-                Bedrooms
-              </label>
+              <label className="formLabel">Bedrooms</label>
               <input
-                id="bedrooms"
-                type="number"
                 className="formInputSmall"
+                type="number"
+                id="bedrooms"
                 value={bedrooms}
                 onChange={onMutate}
                 min="1"
