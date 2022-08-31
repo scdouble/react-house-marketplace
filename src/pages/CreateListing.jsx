9b0e6013353eea4d7c9e94706c74555fc9e78825
Your Name <you@example.com>
2022-08-31 15:17:51 +0900

    Get Coords With Geocoding API

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { toast } from 'react-toastify';
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -43,32 +44,71 @@ function CreateListing() {
   const navigate = useNavigate();
   const isMounted = useRef(true);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData)
+
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error('Discounted price needs to be less than regular price');
+      return;
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error('Max 6 images');
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}}`,
+      );
+
+      const data = await response.json();
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false);
+        toast.error('please enter a correct address');
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+
+    setLoading(false);
   };
 
   const onMutate = (e) => {
-    let boolean = null
-    if(e.target.value ==='true'){
-      boolean = true
+    let boolean = null;
+    if (e.target.value === 'true') {
+      boolean = true;
     }
-    if(e.target.value ==='false'){
-      boolean = false
+    if (e.target.value === 'false') {
+      boolean = false;
     }
 
     // Files
-    if(e.target.files){
-      setFormData((prevState)=>{
-        return { ...prevState, images: e.target.files }
-      })
+    if (e.target.files) {
+      setFormData((prevState) => {
+        return { ...prevState, images: e.target.files };
+      });
     }
 
     // Text
-    if(!e.target.files){
-      setFormData((prevState)=>{
-        return { ...prevState, [e.target.id]:boolean ?? e.target.value }
-      })
+    if (!e.target.files) {
+      setFormData((prevState) => {
+        return { ...prevState, [e.target.id]: boolean ?? e.target.value };
+      });
     }
   };
 
