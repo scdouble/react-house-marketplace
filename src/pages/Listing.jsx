4cdb6c2eb8e3ad07f1f0747ef8487047fe827a4d
Tatsu sei <doubledragontaz@gmail.com>
2022-09-01 22:27:48 +0900

    Explore Slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import SwiperCore, {Navigation, Pagination,Scrollbar, A11y} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -44,7 +45,7 @@ function Listing() {
       <Helmet>
         <title>{listing.name}</title>
       </Helmet>
-      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+      <Swiper  slidesPerView={1}   pagination={{clickable:true}}>
         {listing.imgUrls.map((url, index) => (
           <SwiperSlide key={index}>
             <div
